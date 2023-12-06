341246036bd0878a4a90c2205222dadb78d4b8c6
Dominic009 <dpg11000@gmail.com>
2023-12-06 18:04:23 +0600

    JS

---
+++ practice.js
@@ -0,0 +1,8 @@
+const profile = {
+    name: "Sharaddha Khapra",
+    isFollow : true,
+    posts : 195,
+    followers : 565000,
+    following : 5,
+    description : "Some details about profile holder"  
+}
